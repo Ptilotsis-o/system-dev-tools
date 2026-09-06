e30d54796dc9df244462168614317df8cc8773da
Ptilotsis-o <qiaohaixuan@stu.ouc.edu.cn>
2026-09-06 23:08:56 +0800

    feat(q07): add 'breakpoint()' to 'merge_sort.py'

--- week2/q07/merge_sort.py
+++ week2/q07/merge_sort.py
@@ -2,6 +2,7 @@ def merge(left, right):
     result = []
     i = j = 0
     while i < len(left) and j < len(right):
+        breakpoint()
         if left[i] <= right[j]:
             result.append(left[i]); i += 1
         else:
