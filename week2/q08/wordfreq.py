42c9b45207e3481b7a6b2128ccb8a1156034bc41
Ptilotsis-o <qiaohaixuan@stu.ouc.edu.cn>
2026-09-07 01:13:19 +0800

    feat(q08): optimize 'wordfreq.py'

--- week2/q08/wordfreq.py
+++ week2/q08/wordfreq.py
@@ -1,6 +1,8 @@
 words = open("words.txt", encoding="utf-8").read().split()
 unique = []
+seen = set()
 for word in words:
-    if word not in unique:
+    if word not in seen:
         unique.append(word)
+        seen.add(word)
 print("count=", len(unique))
