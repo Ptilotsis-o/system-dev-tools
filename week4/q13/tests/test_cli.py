6f2e0b31a8b123278bdd8f87e82578ff67683c66
Ptilotsis-o <qiaohaixuan@stu.ouc.edu.cn>
2026-09-09 21:17:14 +0800

    feat(q13): reformated and all tests passed

--- week4/q13/tests/test_cli.py
+++ week4/q13/tests/test_cli.py
@@ -1,15 +1,17 @@
 import sys
 from pathlib import Path
 
+import pytest
+
 src_path = Path(__file__).parent.parent / "src"
 sys.path.insert(0, str(src_path))
 
-import pytest
 from greetlab.cli import main
 
 
 def test_normal_name(capsys):
     sys.argv = ["sdt-greet", "--name", "Alice"]
+    main()
     captured = capsys.readouterr()
     assert captured.out.strip() == "Hello, Alice!"
 
