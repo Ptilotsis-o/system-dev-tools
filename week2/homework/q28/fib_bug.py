d99996e35a72619b8aff4536ad0a7d2d5a8b3e41
Ptilotsis-o <qiaohaixuan@stu.ouc.edu.cn>
2026-09-11 20:43:10 +0800

    feat(q28): finished

--- week2/homework/q28/fib_bug.py
+++ week2/homework/q28/fib_bug.py
@@ -1,6 +1,7 @@
 def fib(n):
-    breakpoint()
-    if n < 2:        # 错误
+    if n == 0:
+        return 0
+    if n < 2:
         return 1
     return fib(n-1) + fib(n-2)
 
---
+++ week2/homework/q28/test_fib.py
@@ -0,0 +1,4 @@
+from fib_bug import fib
+def test_fib0(): assert fib(0) == 0
+def test_fib1(): assert fib(1) == 1
+def test_fib5(): assert fib(5) == 5
