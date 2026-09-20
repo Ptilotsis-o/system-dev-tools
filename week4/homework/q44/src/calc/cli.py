e25d98cf5955cf0f1a11dfb9ef69fefdd9de342e
Ptilotsis-o <qiaohaixuan@stu.ouc.edu.cn>
2026-09-20 20:19:10 +0800

    feat(q44): fix and format

--- week4/homework/q44/src/calc/cli.py
+++ week4/homework/q44/src/calc/cli.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("--op", choices=["add", "sub"], required=True)
@@ -9,5 +10,5 @@ def main():
     if args.op == "add":
         result = args.a + args.b
     else:
-        result = args.a + args.b   # 故意缺陷：减法写成了加法
+        result = args.a - args.b
     print(result)
